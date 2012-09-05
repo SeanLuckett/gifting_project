5334e2a8b6a88352d2d65980e06e693a054ec96e
Sean <sean@quickleft.com>
2012-09-05 09:56:10 -0600

    Set to go.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftola::Application.config.secret_token = '491d1beeaa32d254de64e10d08483c143aa11121d3a730037b7fc39834ea98bb33bc0c17e296eba9f587c3b4d188ab792c8c299b9952371dcbfb85e60ada9d73'
