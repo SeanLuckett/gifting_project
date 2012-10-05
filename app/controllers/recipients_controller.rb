ef678012cf7058302cea82fbb005591780a4c737
Sean <sean@quickleft.com>
2012-10-05 11:44:13 -0600

    Finally saves record correctly, with no mass assignment errors.

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -5,20 +5,16 @@ class RecipientsController < ApplicationController
 
   def create
     user = User.find(params[:user_id])
-    user.recipients.build(pick(params, :fb_id, :birthday, :name, :image))
-    # recipient = user.recipients.find_or_initialize_by_fb_id(params[:recipient][:fb_id])
-    # recipient.attributes = {
-    #   :fb_id => params[:recipient][:fb_id],
-    #   :name => params[:recipient][:name],
-    #   :birthday => params[:recipient][:birthday],
-    #   # :user_id => params[:recipient][:user_id],
-    #   :image => params[:recipient][:image]
-    # }
-    # recipient.save
-    
+    rec = user.recipients.new(pick(params, :fb_id, :birthday, :name, :image))
+    rec.save!
+
+    respond_to do |format|
+      format.json { render :json => user.recipients }
     end
 
-private
+  end
+
+  private
 
   # To avoid mass assignment errors with backbone.js data, this cleans
   # params and returns only those attributes specified with *keys
