d0e8e0dab1fa890da47d0f630b61f3bbf4b261c6
Sean <sean@quickleft.com>
2012-10-25 17:31:41 -0600

    Reconfigures sengrid to work without exposing environment vars.

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,9 +1,9 @@
-SENDGRID_CONFIG = YAML::load(File.open(File.join(Rails.root, "/config/sendgrid.yml")))[Rails.env]
+# SENDGRID_CONFIG = YAML::load(File.open(File.join(Rails.root, "/config/sendgrid.yml")))[Rails.env]
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => SENDGRID_CONFIG["user_name"],
-  :password => SENDGRID_CONFIG["password"],
-  :domain => SENDGRID_CONFIG["domain"],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => ENV['SENDGRID_DOMAIN'],
   :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
