d2cc5bc9e0b375ff9bf0d1fbd866d8e2131d91e5
Sean <sean@quickleft.com>
2012-10-26 09:24:41 -0600

    Sendgrid set up and sends welcome email.

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,5 +1,3 @@
-# SENDGRID_CONFIG = YAML::load(File.open(File.join(Rails.root, "/config/sendgrid.yml")))[Rails.env]
-
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
