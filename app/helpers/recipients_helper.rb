0b5bfbc3e77d73b04ef30f4210a31deb4096333f
Sean <sean@quickleft.com>
2012-11-02 16:38:26 -0600

    Restyles recipient#index

--- app/helpers/recipients_helper.rb
+++ app/helpers/recipients_helper.rb
@@ -1,2 +1,9 @@
 module RecipientsHelper
+  def display_recipient_birthday(recipient)
+    if recipient.birthday
+      return recipient.birthday.to_s(:long)
+    else
+      return "Not entered"
+    end
+  end
 end
