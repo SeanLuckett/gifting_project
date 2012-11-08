56316d55e6896dfab85d34448804b5c27d737dd7
Sean <sean@quickleft.com>
2012-11-08 14:37:22 -0700

    Configures heroku for mailer (I hope).

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,8 @@ require File.expand_path('../application', __FILE__)
 heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
 load(heroku_env) if File.exists?(heroku_env)
 
+# Set mailer default host.
+config.action_mailer.default_url_options = {:host => 'giftola.herokuapp.com'}
+
 # Initialize the rails application
 Giftola::Application.initialize!
