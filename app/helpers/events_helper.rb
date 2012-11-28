7f412a77be558d112340e62555e9d7b8182f1e84
Sean <sean@quickleft.com>
2012-11-27 17:34:47 -0700

    Adds recipient missing data warnings to event index and edit.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def display_recipient(recipient)
+    if recipient.warnings.blank?
+      link_to image_tag(recipient.image), edit_user_recipient_path(@user, recipient)
+    else
+      link_to image_tag(recipient.image, :class => "has-warnings"), edit_user_recipient_path(@user, recipient)
+    end
+  end
 end
