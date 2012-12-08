6f7afe8fe83db1c7cd0826ccc14f20765453db74
Sean <sean@quickleft.com>
2012-12-07 17:44:19 -0700

    Adds tests to facebook authorization.

--- spec/requests/confirm_email_spec.rb
+++ spec/requests/confirm_email_spec.rb
@@ -4,33 +4,38 @@ describe "ConfirmEmail" do
   subject { page } 
 
   describe "#confirm_address" do
-    before { sign_in_facebook_user }
-    let(:user) { User.find_by_name("User Mcfoo") }
-
-    context "when user has no stored email address" do
-      # sets email to "" and reloads page
-      before { user.update_attributes(:email => ""); visit current_path}
-      let(:field_value) { find_field('user[email]').value }
-
-      it { field_value.should be_empty }
+    before :each do
+      set_omniauth :facebook => { :email => nil, :name => "User McFoo" }
+      visit root_path
+      click_link("Sign in with Facebook")
     end
 
-    context "when routing to next step" do
-      context "when user hasn't imported facebook friends" do
-        it "routes to /recipients/import_friends" do
-          click_button 'Confirm'
-          current_path.should == import_friends_path
-          page.should have_content "Email address saved."
-        end
+    context "when user has no stored email address" do
+
+      let(:field_value) { find_field('user[email]').value }
+
+      it { field_value.should be_nil }
+    end
+
+    context "when user hasn't imported facebook friends" do
+      it "routes to /recipients/import_friends" do
+        click_button 'Confirm'
+        current_path.should == import_friends_path
+        page.should have_content "Email address saved."
+      end
+    end
+
+    context "when user has imported facebook friends" do
+      before do
+        user = User.find_by_name("User McFoo")
+        user.update_attributes(:email => "user@example.com")
+        user.recipients.create(:name => "Harry")
+        visit current_path
       end
 
-      context "when user has imported facebook friends" do
-        before { user.recipients.create(:name => "Harry") }
-
-        it "routes to users dashboard" do
-          click_button 'Confirm'
-          current_path.should == dashboard_index_path
-        end
+      it "routes to users dashboard" do
+        click_button 'Confirm'
+        current_path.should == dashboard_index_path
       end
     end
   end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,14 +5,28 @@ describe "StaticPages" do
 
   describe "logging in" do
     context "when authenticated user" do
-      before do
-        set_omniauth
-        visit root_path
+      context "when user has no email imported" do
+        before do
+          set_omniauth :facebook => { :email => nil }
+          visit root_path
+        end
+
+        it "routes to /static_pages/email_confirm after authentication" do
+          click_link("Sign in with Facebook")
+          current_path.should == confirm_email_confirm_address_path
+        end
       end
 
-      it "routes to /static_pages/email_confirm after authentication" do
-        click_link("Sign in with Facebook")
-        current_path.should == confirm_email_confirm_address_path
+      context "when user has an email imported" do
+        before do
+          set_omniauth
+          visit root_path
+        end
+
+        it "goes to import Facebook friends page" do
+          click_link("Sign in with Facebook")
+          current_path.should == import_friends_path
+        end
       end
     end
   end
